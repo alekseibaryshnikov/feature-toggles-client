546d4f6269424f470d1b89c288af288a0e78683d
Aleksei Baryshnikov <a250188@yandex.ru>
2020-08-31 22:48:59 +0300

    Refactoring.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FeatureComponent } from './feature-list/feature/feature.component';
+import { FeatureComponent } from './components/feature-list/feature/feature.component';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatCardModule } from '@angular/material/card';
-import { FeatureListComponent } from './feature-list/feature-list.component';
+import { FeatureListComponent } from './components/feature-list/feature-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatGridListModule } from '@angular/material/grid-list';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
-
-
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { SearchComponent } from './components/search/search.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeatureComponent,
-    FeatureListComponent
+    FeatureListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +31,8 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     HttpClientModule,
     MatNativeDateModule,
     MatCheckboxModule,
-    MatGridListModule
+    MatGridListModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
